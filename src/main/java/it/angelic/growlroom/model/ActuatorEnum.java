eb525f4e4aae3d86200cff0c15c3374f36ce8174
shineangelic <shineangelic@gmail.com>
2020-04-11 01:10:17 +0200

    @JsonCreator

--- src/main/java/it/angelic/growlroom/model/ActuatorEnum.java
+++ src/main/java/it/angelic/growlroom/model/ActuatorEnum.java
@@ -1,5 +1,31 @@
 package it.angelic.growlroom.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public enum ActuatorEnum {
-	FAN, LIGHT, HUMIDIFIER, HEATING;
+	FAN('F'), LIGHT('L'), HUMIDIFIER('M'), HVAC('H');
+
+	char typ;
+
+	private ActuatorEnum(char inVal) {
+		this.typ = inVal;
+	}
+
+	// @JsonValue
+	public long getTyp() {
+		return typ;
+	}
+
+	@JsonCreator
+	public static ActuatorEnum forValues(@JsonProperty("name") String unit) {
+
+		for (ActuatorEnum distance : ActuatorEnum.values()) {
+			if (distance.getTyp() == Character.valueOf(unit.charAt(0))) {
+				return distance;
+			}
+		}
+
+		return null;
+	}
 }
