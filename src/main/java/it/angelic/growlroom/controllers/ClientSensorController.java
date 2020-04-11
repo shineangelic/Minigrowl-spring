ca15e2657bb7116512c91c49601edf4337cebd03
shineangelic <shineangelic@gmail.com>
2020-04-11 02:20:04 +0200

    ESP APIs review

--- src/main/java/it/angelic/growlroom/controllers/ClientSensorController.java
+++ src/main/java/it/angelic/growlroom/controllers/ClientSensorController.java
@@ -11,11 +11,16 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import it.angelic.growlroom.components.SensorsService;
+import it.angelic.growlroom.model.Command;
+import it.angelic.growlroom.service.ActuatorsService;
+import it.angelic.growlroom.service.CommandsService;
+import it.angelic.growlroom.service.SensorsService;
 
 /**
  * L'esp periodicamente contatta il cloud, invia lo stato dei sensori e degli attuatori. Legge una list per scaricare i
@@ -29,14 +34,40 @@ import it.angelic.growlroom.components.SensorsService;
 public class ClientSensorController {
 
 	@Autowired
-	private SensorsService sensorRepository;
+	private SensorsService sensorService;
+	
+	@Autowired
+	private CommandsService commandsService;
+	
+	@Autowired
+	private ActuatorsService actuatorsService;
 
 	@CrossOrigin
 	@GetMapping(value = "/sensors", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Object> getSensors(@RequestParam(value = "dataInizio", required = false) Date dtIn)
 			throws FileNotFoundException, PrintException {
 		 
-		return new ResponseEntity<>(sensorRepository.getSensors(), HttpStatus.OK);
+		return new ResponseEntity<>(sensorService.getSensors(), HttpStatus.OK);
+	}
+	
+	@CrossOrigin
+	@GetMapping(value = "/actuators", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Object> getSensors() {
+		 
+		return new ResponseEntity<>(actuatorsService.getActuators(), HttpStatus.OK);
+	}
+	
+	@CrossOrigin
+	@GetMapping(value = "/commands", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Object> getSupportedCommands() {
+		return new ResponseEntity<>(commandsService.getSupportedCommands(), HttpStatus.OK);
+	}
+	
+	@PutMapping(value = "/commands/queue/add", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	// @ResponseStatus(HttpStatus.OK)
+	public Long sendCommand(@RequestBody Command sensing) {
+		
+		return commandsService.sendCommand(sensing);
 	}
 
 }
---
+++ src/main/java/it/angelic/growlroom/controllers/ESPCommandsController.java
@@ -0,0 +1,50 @@
+package it.angelic.growlroom.controllers;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import it.angelic.growlroom.model.Command;
+import it.angelic.growlroom.service.ActuatorsService;
+import it.angelic.growlroom.service.CommandsService;
+
+/**
+ * L'esp periodicamente contatta il cloud, invia lo stato dei sensori e degli attuatori. Legge una list per scaricare i
+ * comandi da eseguire. E` stupido
+ * 
+ * @author Ale
+ *
+ */
+@RestController
+@RequestMapping(value = "/api/esp/v1/commands")
+public class ESPCommandsController {
+
+	@Autowired
+	private ActuatorsService actuatorsService;
+
+	@Autowired
+	private CommandsService commandsService;
+
+	@CrossOrigin
+	@GetMapping(value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
+	public Collection<Command> downloadComandi() {
+		return commandsService.getUnexecutedCommands();
+	}
+
+	@RequestMapping(method = RequestMethod.DELETE, value = "/id/{id}", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+	public boolean deleteExecutedCommand(@PathVariable String queueCommandid) {
+		try {
+			return commandsService.removeExecutedCommand(Long.valueOf(queueCommandid));
+		} catch (NumberFormatException e) {
+			throw new IllegalArgumentException();
+		}
+	}
+
+}
