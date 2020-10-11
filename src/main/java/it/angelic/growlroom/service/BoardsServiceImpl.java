734620f04453aea550307e21e1f7ebbda906d071
shineangelic <shineangelic@gmail.com>
2020-10-11 17:54:01 +0200

    bugfix

--- src/main/java/it/angelic/growlroom/service/BoardsServiceImpl.java
+++ src/main/java/it/angelic/growlroom/service/BoardsServiceImpl.java
@@ -6,12 +6,14 @@ import java.util.Date;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import it.angelic.growlroom.model.Actuator;
 import it.angelic.growlroom.model.Board;
 import it.angelic.growlroom.model.Sensor;
 import it.angelic.growlroom.model.repositories.BoardsRepository;
 
+@Service
 public class BoardsServiceImpl implements BoardsService {
 	Logger logger = LoggerFactory.getLogger(BoardsServiceImpl.class);
 	@Autowired
